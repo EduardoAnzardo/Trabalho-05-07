ee149f4d7ec2e32c404a80a3f736a1b743a9fbfc
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2022-07-05 19:57:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     MediaAluno M = new MediaAluno();
-    M.Nota1 = 0;
-    M.Nota2 = 0;
+    M.NotaTrabalho = 0;
+    M.NotaProva = 0;
     M.Nota3 = 0;
     M.MediaFinal = 0;
+    M.PesoTrabalho = 0;
+    M.PesoProva = 0;
+    M.PesoNota3 = 0;
     M.Status();
     System.out.println(M);
   }
