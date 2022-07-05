c8ce2d3e78986181addad5b813f9eeaa7ab17f04
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2022-07-05 16:16:28 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    MediaAluno M = new MediaAluno();
+    M.Nota1 = 0;
+    M.Nota2 = 0;
+    M.Nota3 = 0;
+    M.MediaFinal = 0;
+    M.Status();
+    System.out.println(M);
+  }
+}
